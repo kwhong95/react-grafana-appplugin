870bc47edc2ae37d64c9bc76b26d685c3a7c9d7d
kwhong95 <kwhong95@kakao.com>
2021-01-24 00:12:47 +0900

    Grafana PlugIn

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
 import React, { FC } from 'react';
 
+import './App.css';
+
 const App: FC = () => {
   return (
     <div className="root">
-      Grafana PlugIn!
+      <iframe 
+        title="Item1"
+        src="http://localhost:3000/d-solo/LtEIfyBGz/mini-view?orgId=1&refresh=5s&panelId=2" 
+        width="50%" 
+        height="50%" 
+        frameBorder="0"
+      />
+      <iframe 
+        title="Item2"
+        src="http://localhost:3000/d-solo/LtEIfyBGz/mini-view?orgId=1&refresh=5s&panelId=5" 
+        width="50%" 
+        height="50%" 
+        frameBorder="0"
+      />
+      <iframe 
+        title="Item3"
+        src="http://localhost:3000/d-solo/LtEIfyBGz/mini-view?orgId=1&refresh=5s&panelId=4" 
+        width="50%" 
+        height="50%" 
+        frameBorder="0"
+      />
+      <iframe
+        title="Item4" 
+        src="http://localhost:3000/d-solo/LtEIfyBGz/mini-view?orgId=1&refresh=5s&panelId=6" 
+        width="50%" 
+        height="50%" 
+        frameBorder="0"
+      />
     </div>
   )
 }
